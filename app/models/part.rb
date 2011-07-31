24d9df3896e4fe976415ffb6b865cd0515ceda48
Jeremiah Hemphill <jeremiah.hemphill@gmail.com>
2011-07-31 09:51:13 -0400

    Added feature to rename all files when they are uploaded.

--- app/models/part.rb
+++ app/models/part.rb
@@ -2,9 +2,24 @@ class Part < ActiveRecord::Base
   belongs_to :instrument
   belongs_to :piece
 
+  # note the attached file must be a pdf or it probably won't work
   has_attached_file :part_file,
     :s3_headers => { 'Content-Disposition' => 'attachment' },
-    :styles => { :preview => { :geometry => '135',  :format => :png } }
+    :styles => { :preview => { :geometry => '135',  :format => :png } },
+    :path => ":rails_root/public/system/:attachment/:id/:style/:normalized_video_file_name",
+    :url => "/system/:attachment/:id/:style/:normalized_video_file_name" 
+
+  # use an interpolation to set the file name
+  Paperclip.interpolates :normalized_video_file_name do |attachment, style|
+    attachment.instance.normalized_video_file_name
+  end
+
+  # set the file name of the uploaded file to something standardized
+  def normalized_video_file_name
+    piece_name = piece.name.gsub(/[^a-zA-Z0-9_\.]/, '_')
+    instrument_name = instrument.name.gsub(/[^a-zA-Z0-9_\.]/, '_')
+    return "#{piece_name}-#{instrument_name}-#{self.id}.pdf" 
+  end
 
   validates_presence_of :instrument_id
   validates_presence_of :piece_id
